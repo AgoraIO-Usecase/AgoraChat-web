378158dfa8b70f3585f48a232e548e72bc6723f5
逯雷雨 <444247905@qq.com>
2022-03-08 18:26:31 +0800

    Revert "的点点滴滴"

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -1,17 +1,13 @@
-import React, { useState,useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/appbar'
 import './login.css'
 import { loginWithToken } from '../api/loginChat'
 import WebIM from '../utils/WebIM';
 // import { EaseApp } from 'agora-chat-uikit'
 import { EaseApp } from 'luleiyu-agora-chat'
-// import { EaseApp } from 'chat-uikit'
 import { createHashHistory } from 'history'
 import store from '../redux/store'
 import { setMyUserInfo} from '../redux/actions'
-import SessionInfoPopover from '../components/appbar/sessionInfo'
-import GroupMemberInfoPopover from '../components/appbar/chatGroup/memberInfo'
-import { truncate } from 'lodash';
 const history = createHashHistory()
 
 export default function Main() {
@@ -26,46 +22,11 @@ export default function Main() {
             history.push('/login')  
         }
     }, [])
-
-    const [sessionInfoAddEl, setSessionInfoAddEl] = useState(null)
-    const [sessionInfo, setSessionInfo] = useState({});
-
-    const [groupMemberInfoAddEl, setGroupMemberInfoAddEl] = useState(null)
-    const [memberInfo, setMemberInfo] = useState({})
-
-    // session avatar click
-    const handleClickSessionInfoDialog = (e,res) => {
-        // TODO 
-        let isSingleChat = res.chatType === "singleChat"
-        if (isSingleChat) {
-            setSessionInfoAddEl(e.target);
-            setSessionInfo(res)
-        }
-    }
-
-    const handleClickGroupMemberInfoDialog = (e,res) => {
-        let isGroupChat = res.chatType === "groupChat"
-        if (isGroupChat) {
-            setGroupMemberInfoAddEl(e.target);
-            setMemberInfo(res)
-        }
-    }
-
     return (
         <div className='main-container'>
             <EaseApp
                 header={<Header />}
-                onChatAvatarClick={handleClickSessionInfoDialog}
-                onAvatarChange={handleClickGroupMemberInfoDialog}
             />
-            <SessionInfoPopover 
-                open={sessionInfoAddEl}
-                onClose={() => setSessionInfoAddEl(null)}
-                sessionInfo={sessionInfo}/>
-            <GroupMemberInfoPopover 
-                open={groupMemberInfoAddEl}
-                onClose={() => setGroupMemberInfoAddEl(null)}
-                memberInfo={memberInfo}/>
         </div>
     )
 }
