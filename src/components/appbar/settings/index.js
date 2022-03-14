e905e046488015f436f4eb15f696b6d9edac0948
逯雷雨 <444247905@qq.com>
2022-03-14 11:06:18 +0800

    feat: 增加push demo代码

--- src/components/appbar/settings/index.js
+++ src/components/appbar/settings/index.js
@@ -13,13 +13,14 @@ import avater1 from '../../../assets/avatar1.png'
 import avater2 from '../../../assets/avatar2.png'
 import avater3 from '../../../assets/avatar3.png'
 import CheckIcon from '@material-ui/icons/Check';
+import arrow from '../../../assets/go@2x.png'
 
 import { useSelector } from 'react-redux'
 import { setMyUserInfo } from '../../../redux/actions'
 import store from '../../../redux/store'
 
 import { removeFromBlackList } from '../../../api/contactsChat/getContacts'
-const label = { inputProps: { 'aria-label': 'Switch demo' } }
+
 const useStyles = makeStyles((theme) => {
     return {
         root: {
@@ -120,15 +121,16 @@ const useStyles = makeStyles((theme) => {
             backgroundColor: '#F4F5F7',
             borderRadius: '16px',
             width: '100%',
-            padding: '0 16px',
             boxSizing: 'border-box',
         },
         notifyTitle: {
             fontWeight: '600',
             fontSize: '16px',
-            lineHeight: '22px',
+            lineHeight: '54px',
             color: '#0D0D0D',
-
+            borderBottom: '1px solid #E6E6E6',
+            height: '54px',
+            padding: '0 16px',
         },
         notifySubTitle: {
             fontWeight: '600',
@@ -136,13 +138,106 @@ const useStyles = makeStyles((theme) => {
             lineHeight: '22px',
             color: '#0D0D0D',
         },
+        selectAndRadio: {
+            margin: '0 16px',
+        },
+        arrowImg: {
+            width: '20px',
+            height: '20px',
+            cursor: 'pointer',
+        },
+        arrowDownImg: {
+            transform: 'rotate(90deg)',
+        },
+        arrowUpImg: {
+            transform: 'rotate(-90deg)',
+        },
         notifySelect: {
             background: '#fff',
             width: '160px',
             height: '40px',
+        },
+        radioColor: {
+            '&.Mui-checked': {
+                color: '#005FFF',
+            }
+        },
+        previewStyle: {
+            borderTop: '1px solid #E6E6E6'
+        },
+        bottomStyle: {
+            height: '52px',
+        },
+        alertStyle: {
+            margin: '0 16px',
+        },
+        bottomItem: {
+            marginTop: '10px',
+        },
+        flexBox: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+        },
+        notifyPrayTitle: {
+            color: 'rgb(153, 153, 153)',
+            fontSize: '14px',
+            fontWeight: '600',
+            marginLeft: '5px',
+        },
+        btnBox: {
+            width: '100%',
+            textAlign: 'right'
+        },
+        btnStyle: {
+            background: '#114EFF',
+            borderRadius: '26px',
+            width: '84px',
+            height: '28px',
+            color: '#fff',
+            display: 'inline-block',
+            textAlign: 'center',
+            lineHeight: '28px',
+            cursor: 'pointer',
+        },
+        turnStyle: {
+            fontWeight: '500',
+            fontSize: '14px',
+            textAlign: 'right',
+            color: '#114EFF',
+            cursor: 'pointer',
+        },
+        switchStyle: {
+            '& .Mui-checked': {
+                color: '#fff',
+            }
+        },
+        switchOpenStyle: {
+            '& .MuiSwitch-track': {
+                background: 'rgb(48, 78, 238) !important',
+                opacity: 1,
+            }
+        },
+        contentBox: {
+            margin: '20px',
+            fontSize: '14px',
+        },
+        turnOffBtnStyle: {
+            width: '84px',
+            height: '36px',
+            color: '#000',
+            display: 'inline-block',
+            textAlign: 'center',
+            lineHeight: '36px',
+            cursor: 'pointer',
+            fontSize: '14px',
+        },
+        rightBtn: {
+            margin: '0px 20px 20px 10px',
+            fontSize: '14px',
         }
-    };
-});
+    }
+})
 
 const AVATARS = [avater1, avater2, avater3]
 export default function Setting({ open, onClose }) {
@@ -152,7 +247,13 @@ export default function Setting({ open, onClose }) {
     const [nickName, setNickName] = useState('')
     const [avatarIndex, setAvatarIndex] = useState(null)
     const [addEl, setAddEl] = useState(null)
-    const [age, setAge] = React.useState('');
+    const [notifyText, setNotifyText] = React.useState('');
+    const [defaultValue, setDefaultValue] = useState('')
+    const [showRadio, setShowRadio] = useState(false)
+    const [checkedValue, setCheckedValue] = useState('')
+    const [textSwitch, setTextSwitch] = useState(false)
+    const [soundSwitch, setSoundSwitch] = useState(false)
+    const [openTurnOff, setopenTurnOff] = useState(false)
 
     const myUserInfo = useSelector(state => state?.myUserInfo)
     const blackList = useSelector(state => state?.blackList) || []
@@ -248,10 +349,67 @@ export default function Setting({ open, onClose }) {
         }
     }
 
-    const handleChange = (event) => {
-        setAge(event.target.value)
+    const handleSelectChange = (event) => {
+        setNotifyText(event.target.value)
+    }
+    const radioList = [
+        {
+            title: 'For 15 minutes',
+            value: '0'
+        },
+        {
+            title: 'For 1 hour',
+            value: '1'
+        },
+        {
+            title: 'For 8 hours',
+            value: '2'
+        },
+        {
+            title: 'For 24 hours',
+            value: '3'
+        },
+        {
+            title: 'Until 8:00 AM Tomorow',
+            value: '4'
+        },
+        {
+            title: 'Until I turn it off',
+            value: '5'
+        }
+    ]
+
+    const handleChangeRadio = (event) => {
+        setDefaultValue(event.target.value)
+    }
+    const handlerArrowImg = () => {
+        setShowRadio(!showRadio)
+    }
+    const handlerTurnOffBtn = () => {
+        setopenTurnOff(true)
+    }
+    const handlerDoneBtn = () => {
+        setCheckedValue(radioList[Number(defaultValue)].title)
+        setShowRadio(false)
     }
 
+    const handleSwitchChange = (e, val) => {
+        console.log(e, val)
+        const checked = e.target.checked
+        if (val) {
+            setSoundSwitch(checked)
+        } else {
+            setTextSwitch(checked)
+        }
+    }
+
+    const handleTurnOffClose = () => {
+        setopenTurnOff(false)
+    }
+    const handlerOkay = () => {
+        setCheckedValue('')
+        setShowRadio(true)
+    }
     function infoTabPanel() {
         return (
             < div className={
@@ -359,72 +517,102 @@ export default function Setting({ open, onClose }) {
         )
     }
 
+    function renderTurnOffContent() {
+        return (
+            <div className={classes.contentBox}>
+                {`You have set Do Not Disturb${' ' + checkedValue}.`}
+            </div>
+        )
+    }
+
+    function renderTurnOffFooter() {
+        return (
+            <div className={classes.btnBox}>
+                <span className={classes.turnOffBtnStyle} onClick={handleTurnOffClose}>Cancel</span>
+                <span className={classes.btnStyle + ' ' + classes.rightBtn} onClick={handlerOkay}>Okay</span>
+            </div>
+        )
+    }
+
     function notificationTabPanel() {
         return (
             <div className={classes.notificationPanel}>
                 <div className={classes.notificationItem}>
                     <div className={classes.notifyTitle}>
-                        Push Notifications
+                        {i18next.t('Push Notifications')}
                     </div>
-                    <div>
+                    <div className={classes.selectAndRadio + ' ' + classes.bottomItem}>
                         <div>
-                            <div>
-                                <span className={classes.notifySubTitle}>Notifications Settings</span>
+                            <div className={classes.flexBox}>
+                                <span className={classes.notifySubTitle}>{i18next.t('Notifications Settings')}</span>
                                 <Select
-                                    value={age}
+                                    value={notifyText}
                                     className={classes.notifySelect}
-                                    onChange={handleChange}
-                                    displayEmpty
-                                    inputProps={{ 'aria-label': 'Without label' }}
+                                    onChange={handleSelectChange}
                                     variant="outlined"
                                 >
-                                    <MenuItem value={10}>All Message</MenuItem>
-                                    <MenuItem value={20}>Only @Metion</MenuItem>
-                                    <MenuItem value={30}>Nothing</MenuItem>
+                                    <MenuItem value={1}>All Message</MenuItem>
+                                    <MenuItem value={2}>Only @Metion</MenuItem>
+                                    <MenuItem value={3}>Nothing</MenuItem>
                                 </Select>
                             </div>
-                            <div>
-                                <div>
-                                    <span className={classes.notifySubTitle}>Do not Disturb</span>
-                                    <img alt="" src="../../../assets/go@2x.png" />
-                                </div>
-                                <div>
-                                    <RadioGroup
-                                        aria-labelledby="demo-radio-buttons-group-label"
-                                        defaultValue="0"
-                                        name="radio-buttons-group"
-                                    >
-                                        <FormControlLabel value="0" control={<Radio sx={{
-                                            color: '#005FFF',
-                                            '&.Mui-checked': {
-                                                color: '#005FFF',
-                                            },
-                                            }} />} label="For 15 minutes" />
-                                        <FormControlLabel value="1" control={<Radio />} label="For 1 hour" />
-                                        <FormControlLabel value="2" control={<Radio />} label="For 8 hours" />
-                                        <FormControlLabel value="3" control={<Radio />} label="For 24 hours" />
-                                        <FormControlLabel value="4" control={<Radio />} label="Until 8:00 AM Tomorow" />
-                                        <FormControlLabel value="5" control={<Radio />} label="Until I turn it off" />
-                                    </RadioGroup>
+                            <div className={classes.bottomItem}>
+                                <div className={classes.flexBox}>
                                     <div>
-                                        <Button>Done</Button>
+                                        <span className={classes.notifySubTitle}>{i18next.t('Do not Disturb')}</span>
+                                        {
+                                            checkedValue ? <span className={classes.notifyPrayTitle}>{checkedValue}</span> : ''
+                                        }
                                     </div>
+                                    {
+                                        checkedValue ?
+                                        <span onClick={handlerTurnOffBtn} className={classes.turnStyle}>Turn Off</span>
+                                        : <img className={`${classes.arrowImg} ${showRadio ? classes.arrowUpImg : classes.arrowDownImg}`} alt="" onClick={handlerArrowImg} src={arrow} />
+                                    }
                                 </div>
+                                {showRadio ?
+                                    <div className={classes.previewStyle + ' ' + classes.bottomItem}>
+                                        <RadioGroup
+                                            aria-labelledby="demo-radio-buttons-group-label"
+                                            name="radio-buttons-group"
+                                            value={defaultValue}
+                                            onChange={handleChangeRadio}
+                                        >
+                                            {
+                                                radioList.map(item => {
+                                                    return (
+                                                        <FormControlLabel key={item.title} value={item.value} control={<Radio className={classes.radioColor} />} label={item.title} />
+                                                    )
+                                                })
+                                            }
+                                        </RadioGroup>
+                                        <div className={classes.btnBox}>
+                                            <span onClick={handlerDoneBtn} className={classes.btnStyle}>{i18next.t('Done')}</span>
+                                        </div>
+                                    </div> : null
+                                }
                             </div>
                         </div>
-                        <div>
-                            <span className={classes.notifySubTitle}>Show Preview Text</span>
-                            <Switch {...label} defaultChecked></Switch>
+                        <div className={classes.bottomStyle + ' ' + classes.previewStyle + ' ' + classes.bottomItem + ' ' + classes.flexBox}>
+                            <span className={classes.notifySubTitle}>{i18next.t('Show Preview Text')}</span>
+                            <Switch className={`${classes.switchStyle} ${textSwitch ? classes.switchOpenStyle : ''}`} onChange={(e) => handleSwitchChange(e, 0)}></Switch>
                         </div>
                     </div>
                 </div>
-                <div className={classes.notificationItem}>
-                    <div className={classes.notifyTitle}>Notification Sounds</div>
-                    <div>
-                        <span className={classes.notifySubTitle}>Alert Sound</span>
-                        <Switch {...label} defaultChecked></Switch>
+                <div className={classes.notificationItem + ' ' + classes.bottomItem}>
+                    <div className={classes.notifyTitle}>{i18next.t('Notification Sounds')}</div>
+                    <div className={classes.bottomStyle + ' ' + classes.alertStyle + ' ' + classes.flexBox}>
+                        <span className={classes.notifySubTitle}>{i18next.t('Alert Sound')}</span>
+                        <Switch className={`${classes.switchStyle} ${soundSwitch ? classes.switchOpenStyle : ''}`} onChange={(e) => handleSwitchChange(e, 1)}></Switch>
                     </div>
                 </div>
+                <CommonDialog
+                    open={openTurnOff}
+                    onClose={handleTurnOffClose}
+                    title={i18next.t("Turn off Do Not Disturb?")}
+                    content={renderTurnOffContent()}
+                    footer={renderTurnOffFooter()}
+                ></CommonDialog>
             </div>
         )
     }
