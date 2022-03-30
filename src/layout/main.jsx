25bde62f9cf8847d24a6c1ab9a7c16cc2bb65251
lizg <903504686@qq.com>
2022-03-30 18:05:00 +0800

    feat: 支持群组头像点击查看详情

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react';
 import Header from '../components/appbar'
 import './login.css'
 import { loginWithToken } from '../api/loginChat'
+import getGroupInfo from '../api/groupChat/getGroupInfo'
 import WebIM from '../utils/WebIM';
 import { EaseApp } from 'chat-uikit'
 import { createHashHistory } from 'history'
@@ -9,7 +10,7 @@ import store from '../redux/store'
 import { setMyUserInfo} from '../redux/actions'
 import SessionInfoPopover from '../components/appbar/sessionInfo'
 import GroupMemberInfoPopover from '../components/appbar/chatGroup/memberInfo'
-import { truncate } from 'lodash';
+import GroupSettingsDialog from '../components/appbar/chatGroup/groupSettings'
 const history = createHashHistory()
 
 export default function Main() {
@@ -31,13 +32,19 @@ export default function Main() {
     const [groupMemberInfoAddEl, setGroupMemberInfoAddEl] = useState(null)
     const [memberInfo, setMemberInfo] = useState({})
 
+    const [groupSettingAddEl, setGroupSettingAddEl] = useState(null)
+    const [currentGroupId, setCurrentGroupId] = useState("");
+
     // session avatar click
     const handleClickSessionInfoDialog = (e,res) => {
-        // TODO 
-        let isSingleChat = res.chatType === "singleChat"
-        if (isSingleChat) {
+        let {chatType,to} = res
+        if (chatType === "singleChat") {
             setSessionInfoAddEl(e.target);
             setSessionInfo(res)
+        } else if (chatType === "groupChat"){
+            getGroupInfo(to)
+            setGroupSettingAddEl(e.target)
+            setCurrentGroupId(to)
         }
     }
 
@@ -64,6 +71,10 @@ export default function Main() {
                 open={groupMemberInfoAddEl}
                 onClose={() => setGroupMemberInfoAddEl(null)}
                 memberInfo={memberInfo}/>
+            <GroupSettingsDialog 
+                open={groupSettingAddEl}
+                onClose={() => setGroupSettingAddEl(null)}
+                currentGroupId={currentGroupId} />
         </div>
     )
 }
