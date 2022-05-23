da8b9af362de32f4f570de1f00ca22e6f7744361
wangying <wangying@wangyingdeMacBook-Pro.local>
2022-05-23 09:49:12 +0800

    fix: click conversation error

--- src/utils/WebIM.js
+++ src/utils/WebIM.js
@@ -6,7 +6,7 @@
 // import { EaseApp } from "chat-uikit";
 import { EaseApp } from "wy-chat";
 
-const WebIM = EaseApp.getSdk({ appkey: "5101220107132865#test" });
+const WebIM = EaseApp.getSdk({ appkey: "81446724#514456" });
 
 export default WebIM;
 
