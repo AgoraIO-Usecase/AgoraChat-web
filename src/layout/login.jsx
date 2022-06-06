c94e4af0fbc1803e92ebe87e4a3b4b79384dd2a8
myZhangDong <973849793@qq.com>
2022-06-06 16:04:12 +0800

    feat: update login page ui

--- src/layout/login.jsx
+++ src/layout/login.jsx
@@ -8,6 +8,7 @@ import { getToken, loginWithToken, loginWithPassword } from '../api/loginChat'
 import store from '../redux/store'
 import { setMyUserInfo, setFetchingStatus } from '../redux/actions'
 import { message } from '../components/common/alert'
+import WebIM from '../utils/WebIM'
 export default function Login() {
     // const history = useHistory()
     const [notice, setNotice] = useState({
@@ -37,16 +38,41 @@ export default function Login() {
         store.dispatch(setFetchingStatus(true))
         if (values.nickName) {
             getToken(values.agoraId, values.nickName).then((res) => {
-                const { accessToken } = res
+                const { accessToken, agoraUid } = res
+                WebIM.conn.agoraUid = agoraUid
                 loginWithToken(values.agoraId, accessToken)
+                
                 store.dispatch(setMyUserInfo({ agoraId: values.agoraId, nickName: values.nickName }))
-                sessionStorage.setItem('webim_auth', JSON.stringify({ ...values, accessToken }))
+                sessionStorage.setItem('webim_auth', JSON.stringify({ ...values, accessToken, agoraUid }))
             }).catch(() => {
                 store.dispatch(setFetchingStatus(false))
                 message.error('login fail.')
             })
         } else if (values.password) {
-            loginWithPassword(values.agoraId, values.password)
+            // loginWithPassword(values.agoraId, values.password)
+
+            let options = {
+                user: values.agoraId,
+                pwd: values.password
+            };
+
+            WebIM.conn.open(options).then((res) => {
+                const { accessToken } = res
+                store.dispatch(setMyUserInfo({  agoraId: values.agoraId, password: values.password }))
+                sessionStorage.setItem('webim_auth', JSON.stringify({ agoraId: values.agoraId, password: values.password, accessToken:accessToken }))
+            }).catch((err) => {
+                store.dispatch(setFetchingStatus(false))
+                console.log('catch', err)
+                // message.error('login fail.')
+                if(err.type === 1){
+                    let reason = err.message
+                    if(err.data.message){
+                        reason = err.data.message
+                    }
+                    return setNotice({ show: true, text: reason })
+                }
+            })
+
         }
         // getToken(values.agoraId, values.nickName).then((res) => {
         //     const { accessToken } = res
@@ -93,7 +119,7 @@ export default function Login() {
                 onChange={handleChange('agoraId')} value={values.agoraId}></input>
                 <input className='login-form-input' placeholder={i18next.t('login-NickName')} value={values.nickName} onChange={handleChange('nickName')}></input>
                 <input className='login-form-input' placeholder={i18next.t('login-Password')} value={values.password} onChange={handleChange('password')}></input>
-                <input type='button' className='login-form-input button' value={i18next.t('login-Login')} onClick={login} />
+                <input type='button' className='login-form-input button' value={i18next.t('login-Login')} onClick={login} ></input>
             </div>
             <div className='login-copyright'>
                 © 2022 Agora
