d58ef504085a871da23ee6d06ab9ed7afe05e34b
myZhangDong <973849793@qq.com>
2023-08-24 18:25:29 +0800

    fix: close chat when logout

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -239,6 +239,12 @@ function Main() {
     if (data.selectedLang) {
       store.dispatch(setTargetLanguage(data.selectedLang));
     }
+    return () => {
+      rootStore.conversationStore.setCurrentCvs({
+        chatType: "",
+        conversationId: ''
+      })
+    }
   }, []);
 
   const handleTranslateMsg = () => {
@@ -380,7 +386,7 @@ function Main() {
             )}
           ></Chat>
         </div>
-        {
+        {   
           <div
             style={{
               width: "360px",
